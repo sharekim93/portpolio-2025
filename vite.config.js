af22fd2bd06b6f9c65407a9980d7ad0486634f68
sharekim <nanum.kim@chexcar.co.kr>
2025-02-27 16:47:11 +0900

    chore: add shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix(), tailwindcss()],
 });
