b1834fcade75cf8b5ca8df663b7ce3c27c1e4532
sharekim <sksyag@naver.com>
2025-03-22 15:58:31 +0900

    chore: vite config alias 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { resolve } from "path";
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./app"),
+      "@/*": resolve(__dirname, "app/*"),
     },
   },
   plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
