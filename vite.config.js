a4960a5c31d0558f787e2cb1c56c1f5fc1936cce
sharekim <sksyag@naver.com>
2025-03-22 15:56:28 +0900

    chore: vite.config 에 alias 추가

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./app"),
+    },
+  },
   plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
 });
