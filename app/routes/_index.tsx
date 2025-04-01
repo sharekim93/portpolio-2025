9d029be21a5b6d2b071c0a6ef589a2d172f8a5de
sharekim <sksyag@naver.com>
2025-04-01 13:10:47 +0900

    fix: projects-> Projects (Capital)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Suspense, lazy } from "react";
 const HomePage = () => {
   const AboutComponent = lazy(() => import("@/components/About"));
   const ExperienceComponent = lazy(() => import("@/components/Experience"));
-  const ProjectsComponent = lazy(() => import("@/components/projects/index"));
+  const ProjectsComponent = lazy(() => import("@/components/Projects"));
   const FloatingActionButtonComponent = lazy(
     () => import("@/components/molecules/FloatingActionButton")
   );
